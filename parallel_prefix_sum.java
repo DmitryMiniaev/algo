9a9d875bf18180d121112fa2912da8cecd586837
DmitryMiniaev <d.miniaev@gmail.com>
2016-07-07 19:15:11 +0300

    Update parallel_prefix_sum.java

--- parallel_prefix_sum.java
+++ parallel_prefix_sum.java
@@ -1,9 +1,20 @@
-        int[] a = new int[] {6, 4, 16, 10, 16, 14, 2, 8};
-        int tlen = (Integer.highestOneBit(a.length) << 1) - 1;
+        int[] a = new int[10000];
+        int[] b = new int[10000];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rnd.nextInt();
+            b[i] = a[i];
+        }
+//        int[] a = new int[] {6, 4, 16, 10, 16, 14, 2, 8, 1};
+//        int[] b = new int[] {6, 4, 16, 10, 16, 14, 2, 8, 1};
+        int tlen = 1;
+        while (a.length > Integer.highestOneBit(tlen)) {
+            tlen <<= 1;
+            tlen |= 1;
+        }
         int[] sum = new int[tlen];
         int lvlLo = (tlen - tlen / 2 - 1);
         int lvlHi = tlen;
-        for (int i = lvlLo; i < lvlHi; i++) {
+        for (int i = lvlLo; i - lvlLo < a.length ; i++) {
             sum[i] = a[i - lvlLo];
         }
         int lvlCount = Integer.bitCount(tlen) - 1;
@@ -30,6 +41,16 @@
         for (int i = 0; i < a.length; i++) {
             ps[i] = a[i] + fromLeft[offset + i];
         }
-        System.out.println(Arrays.toString(sum));
-        System.out.println(Arrays.toString(fromLeft));
-        System.out.println(Arrays.toString(ps));
+
+        int[] ps2 = new int[a.length];
+        ps2[0] = b[0];
+        for (int i = 1; i < a.length; i++) {
+            ps2[i] = ps2[i - 1] + b[i];
+        }
+//        System.out.println(Arrays.toString(ps));
+//        System.out.println(Arrays.toString(ps2));
+        for (int i = 0; i < a.length; i++) {
+            if(ps[i] != ps2[i]) {
+                throw new RuntimeException();
+            }
+        }
